d6ea0cc6632fdbe9af9c995e0acd7436d9faa547
Ömer Avci <oemer.a@me.com>
2012-03-23 23:24:40 +0100

    added appirater

--- hackful/HackfulAppDelegate.m
+++ hackful/HackfulAppDelegate.m
@@ -10,6 +10,7 @@
 #import "NavigationController.h"
 #import "MainTabBarController.h"
 #import "RootViewController.h"
+#import "Appirater.h"
 
 @implementation HackfulAppDelegate
 
@@ -30,6 +31,8 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    [Appirater appLaunched:YES];
     return YES;
 }
 
@@ -51,9 +54,7 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
+    [Appirater appEnteredForeground:YES];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
